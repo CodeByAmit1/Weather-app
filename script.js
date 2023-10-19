af4e4a3bae33172b771ff367c3b22a7c0f81e763
amitkumar-avviare <amitkumar.avviare@gmail.com>
2023-10-19 05:16:54 -0700

    WEATHER API

---
+++ script.js
@@ -0,0 +1,25 @@
+const apikey = "c78d3b13e27138996c4539e935f99bc2";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
+
+checkWeather();
+
